e8b1291eafe03c3409fa9167a98e3bb2fca1c12b
Alice Boucher <yboucher@nvidia.com>
2026-03-16 10:52:23 -0700

    no presolve

--- benchmarks/linear_programming/cuopt/run_mip.cpp
+++ benchmarks/linear_programming/cuopt/run_mip.cpp
@@ -277,7 +277,7 @@ int run_single_file(std::string file_path,
     settings.work_limit);
   settings.tolerances.relative_tolerance = 1e-12;
   settings.tolerances.absolute_tolerance = 1e-6;
-  settings.presolver                     = cuopt::linear_programming::presolver_t::Default;
+  settings.presolver                     = cuopt::linear_programming::presolver_t::None;
   settings.reliability_branching         = reliability_branching;
   settings.clique_cuts                   = -1;
   settings.seed                          = 42;
